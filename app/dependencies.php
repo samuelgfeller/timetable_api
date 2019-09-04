ec132c5575adee6e257f5f6341e52165a0b62421
Samuel Gfeller <sgfeller@wmc.ch>
2019-09-04 16:18:44 +0200

    fixed dependency

--- app/dependencies.php
+++ app/dependencies.php
@@ -10,27 +10,29 @@ use Psr\Log\LoggerInterface;
 
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
-        LoggerInterface::class => function (ContainerInterface $c) {
-        $settings = $c->get('settings');
-        $loggerSettings = $settings['logger'];
-        $logger = new Logger($loggerSettings['name']);
-
-        $processor = new UidProcessor();
-        $logger->pushProcessor($processor);
-
-        $handler = new StreamHandler($loggerSettings['path'], $loggerSettings['level']);
-        $logger->pushHandler($handler);
-
-        return $logger;
+        LoggerInterface::class => function (ContainerInterface $c)
+        {
+            $settings = $c->get('settings');
+            $loggerSettings = $settings['logger'];
+            $logger = new Logger($loggerSettings['name']);
+        
+            $processor = new UidProcessor();
+            $logger->pushProcessor($processor);
+        
+            $handler = new StreamHandler($loggerSettings['path'], $loggerSettings['level']);
+            $logger->pushHandler($handler);
+        
+            return $logger;
         },
         PDO::class => function (ContainerInterface $c) {
             $dbSettings = $c->get('settings')['db'];
             $dsn = 'mysql:host=' . $dbSettings['host'] . ';dbname=' . $dbSettings['dbname'];
-            $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                PDO::ATTR_EMULATE_PREPARES => false,];
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ];
             return new PDO($dsn, $dbSettings['user'], $dbSettings['pass'], $options);
         },
-
     ]);
 };
